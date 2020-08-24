48b6315d1be453729e9b0a604cd8c2574995e5d8
utsumichikara <chiksrskobu0225@gmail.com>
2020-08-24 06:07:05 +0000

    first commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Potsuri
+  class Application < Rails::Application
+    config.load_defaults 5.2
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+  end
+end
