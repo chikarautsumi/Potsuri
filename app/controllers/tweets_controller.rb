d27dd6db1f8272ace641a75d67f572f774030ca1
chikarautsumi <chiksrskobu0225@gmail.com>
2020-08-25 08:26:30 +0000

    投稿編集機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,6 +22,20 @@ class TweetsController < ApplicationController
         @tweet = Tweet.find(params[:id])
     end
 
+    def edit
+        @tweet = Tweet.find(params[:id])
+    end
+
+    def update
+        @tweet = Tweet.new(tweet_params)
+        if @tweet.save
+            flash[:notice] = '編集完了しました'
+            redirect_to tweets_path
+        else
+            render action: :edit
+        end
+    end
+
     private
     def tweet_params
         params.require(:tweet).permit(:content)
