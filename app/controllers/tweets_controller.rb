b005f5e1d2b7f1e855eb3e1fd28ef6e85dfb078a
chikarautsumi <chiksrskobu0225@gmail.com>
2020-08-25 04:58:39 +0000

    投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,22 @@ class TweetsController < ApplicationController
     def index
         @tweets = Tweet.all
     end
+
+    def new
+        @tweet  = Tweet.new
+    end
+
+    def create
+        @tweet = Tweet.new(tweet_params)
+        if @tweet.save
+            flash[:notice] = '投稿完了しました'
+            redirect_to tweets_path
+        else
+            render action: :new
+        end
+    end
+    private
+    def tweet_params
+        params.require(:tweet).permit(:content)
+    end
 end
